a469f0799f9a09027eea71c1674e9e72d75bf4f8
Azizzarkasyi <koyekkasyi@gmail.com>
2023-10-09 20:34:12 +0700

    macro

---
+++ knowlage/app/src/main/java/com/example/knowlage/daftar_akun_Pelamar.kt
@@ -0,0 +1,11 @@
+package com.example.knowlage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class daftar_akun_Pelamar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_akun_pelamar)
+    }
+}
