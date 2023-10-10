1f5c0b05f5e9612c02497db98f57acb3bb75d6f4
Azizzarkasyi <koyekkasyi@gmail.com>
2023-10-10 19:08:47 +0700

    update

--- knowlage/app/build.gradle.kts
+++ knowlage/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.knowlage"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.knowlage"
@@ -34,7 +34,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.5.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
